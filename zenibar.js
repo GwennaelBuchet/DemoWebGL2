6ce3e735c3ff7d509274240816d53f1e22026be3
Gwennael Buchet <gwennael.buchet@gmail.com>
2018-12-29 18:47:34 +0100

    [Add] a grid in the scene

--- zenibar.js
+++ zenibar.js
@@ -15,6 +15,7 @@ function main() {
 		vertexPosition: gl.getAttribLocation(shaderProgram, 'aVertexPosition'),
 		vertexNormal: gl.getAttribLocation(shaderProgram, 'aVertexNormal'),
 		textureCoord: gl.getAttribLocation(shaderProgram, 'aTextureCoord'),
+		vertexColor: gl.getAttribLocation(shaderProgram, 'aVertexColor'),
 
 		projectionMatrix: gl.getUniformLocation(shaderProgram, 'uProjectionMatrix'),
 		modelViewMatrix: gl.getUniformLocation(shaderProgram, 'uModelViewMatrix'),
@@ -25,6 +26,7 @@ function main() {
 
 	// set the geometry definition
 	const cubeBuffers = initCubeBuffers(gl);
+	const gridBuffers = initGridBuffers(gl);
 	let meshBottle;
 
 	const texture1 = loadTexture(gl, "assets/biere-mousse-carre.jpg");
@@ -39,11 +41,10 @@ function main() {
 	// let's render.
 	// As it's now animated, we use the requestAnimationFrame function to smooth it up
 	function render() {
-		drawScene(gl, shaderProgramParams, meshBottle, texture1, texture2);
+		drawScene(gl, shaderProgramParams, gridBuffers, texture2, texture1);
 
 		requestAnimationFrame(render);
 	}
-
 
 }
 
@@ -279,6 +280,93 @@ function initCubeBuffers(gl) {
 }
 
 /**
+ * Initialize the buffers for the Cube we'll display
+ * @param gl {WebGLRenderingContext}
+ * @returns {{position: WebGLBuffer, textureCoord: WebGLBuffer, normal: WebGLBuffer, indices: WebGLBuffer}}
+ */
+function initGridBuffers(gl) {
+
+	// Buffer for the cube's vertices positions.
+	const positionsBuffer = gl.createBuffer();
+	// Buffer to hold indices into the vertex array for each faces's vertices.
+	const indicesBuffer = gl.createBuffer();
+	// Buffer for normals
+	const normalBuffer = gl.createBuffer();
+	// Buffer for colors of each vertex of each face
+	const colorsBuffer = gl.createBuffer();
+	// Buffer for texture coordinates
+	const textureCoordBuffer = gl.createBuffer();
+
+
+	// Define the position for each vertex of each face
+	let positions = [];
+	let vertexNormals = [];
+	let indices = [];
+	let colors = [];
+	let textureCoordinates = [];
+
+	let width = 0.4; // width of a square
+	let nbW = 50; // number of squares on X axis
+	let nbH = 50; // number of squares on Z axis
+	let index = 0;
+	let startX = -nbW * width / 2.; // try to center the grid by translating its start point half its size
+	let startY = 0.;
+	let startZ = -nbH * width / 2.;
+	let c = [0.878, 0.592, 0.400, 1.0]; // orange
+
+	// fill positions, normals, uv, colors and indices in a single double-loop
+	for (let h = 0; h <= nbH; h++) {
+		for (let w = 0; w <= nbW; w++) {
+			positions[index + 0] = startX + w * width; // x
+			positions[index + 1] = startY; // y
+			positions[index + 2] = startZ + h * width; // z
+
+			vertexNormals[index + 0] = 0.0; // x
+			vertexNormals[index + 1] = Math.random();  //1.0; // y
+			vertexNormals[index + 2] = 0.0; // z
+
+			colors = colors.concat(c);
+
+			textureCoordinates = textureCoordinates.concat(w / nbW, h / nbH); // u, v
+
+
+			// Each square of the grid is composed by 2 triangles.
+			// Each triangle is composed with 3 vertices. "indices" stores the indices in the "positions" array of these vertices, counter-clock wise.
+			if (w < nbW && h < nbH) {
+				let i = w + (h * (nbW+1));
+				indices = indices.concat(i, i + nbW + 1, i + nbW + 2); // 1st triangle of the square
+				indices = indices.concat(i, i + nbW + 2, i + 1); // 2nd triangle of the square
+			}
+			index += 3;
+		}
+	}
+
+	gl.bindBuffer(gl.ARRAY_BUFFER, positionsBuffer);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+
+	gl.bindBuffer(gl.ARRAY_BUFFER, colorsBuffer);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+
+	gl.bindBuffer(gl.ARRAY_BUFFER, textureCoordBuffer);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(textureCoordinates), gl.STATIC_DRAW);
+
+	gl.bindBuffer(gl.ARRAY_BUFFER, normalBuffer);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexNormals), gl.STATIC_DRAW);
+
+	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indicesBuffer);
+	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
+
+	return {
+		verticesBuffer: positionsBuffer,
+		textureCoordBuffer: textureCoordBuffer,
+		colorsBuffer: colorsBuffer,
+		normalsBuffer: normalBuffer,
+		indicesBuffer: indicesBuffer,
+		count: indices.length
+	};
+}
+
+/**
  *
  * @param gl {WebGLRenderingContext}
  * @param url {String}
@@ -349,7 +437,8 @@ function drawScene(gl, shaderProgramParams, mesh, texture1, texture2) {
 	                 zNear,
 	                 zFar);
 
-	drawMesh(gl, projectionMatrix, shaderProgramParams, mesh, texture1, [0.0, 0.0, -40.0], [0, -1 * time, 0]);
+	drawMesh(gl, projectionMatrix, shaderProgramParams, mesh, texture1, [0.0, 0.0, -30.0], [0.1, 0, 0]);
+	//drawMesh(gl, projectionMatrix, shaderProgramParams, mesh, texture1, [0.0, 0.0, -20.0], [0, -1 * time, 0.5]);
 	//drawMesh(gl, projectionMatrix, shaderProgramParams, buffers, texture2, [3, 0.0, -15.0], [0, 1 * time, 0.5 * time]);
 
 	time += 0.01;
@@ -383,7 +472,7 @@ function drawMesh(gl, projectionMatrix, shaderProgramParams, mesh, texture, tran
 	gl.bindBuffer(gl.ARRAY_BUFFER, mesh.verticesBuffer);
 	gl.vertexAttribPointer(
 		shaderProgramParams.vertexPosition,
-		3,      // size
+		3,      // size : X,Y,Z = 3 values
 		gl.FLOAT,       // type
 		false, // normalized
 		0,      // stride
@@ -395,7 +484,7 @@ function drawMesh(gl, projectionMatrix, shaderProgramParams, mesh, texture, tran
 	gl.bindBuffer(gl.ARRAY_BUFFER, mesh.textureCoordBuffer);
 	gl.vertexAttribPointer(
 		shaderProgramParams.textureCoord,
-		2, // size
+		2, // size : U,V = 2 values
 		gl.FLOAT, // type
 		false, //normalized
 		0, // stride
@@ -407,11 +496,23 @@ function drawMesh(gl, projectionMatrix, shaderProgramParams, mesh, texture, tran
 	// Bind the texture to texture unit 0
 	gl.bindTexture(gl.TEXTURE_2D, texture);
 
+	// Set the vertexColor attribute of the shader
+	gl.bindBuffer(gl.ARRAY_BUFFER, mesh.colorsBuffer);
+	gl.vertexAttribPointer(
+		shaderProgramParams.vertexColor,
+		4, // size : R,G,B,A = 4 values
+		gl.FLOAT, // type
+		false, // normalized
+		0, // stride
+		0 // offset
+	);
+	gl.enableVertexAttribArray(shaderProgramParams.vertexColor);
+
 	// Normals
 	gl.bindBuffer(gl.ARRAY_BUFFER, mesh.normalsBuffer);
 	gl.vertexAttribPointer(
 		shaderProgramParams.vertexNormal,
-		3, // size
+		3, // size : X,Y,Z = 3 values
 		gl.FLOAT, // type
 		false, // normalized
 		0, //stride
@@ -442,7 +543,7 @@ function drawMesh(gl, projectionMatrix, shaderProgramParams, mesh, texture, tran
 
 	// Let's render
 	gl.drawElements(gl.TRIANGLES,
-	                mesh.mesh.vertices.length, // count (number of vertices)
+	                mesh.count, //mesh.vertices.length, // count (number of vertices)
 	                gl.UNSIGNED_SHORT, // type
 	                0 // offset
 	);
@@ -505,7 +606,7 @@ function createProgram(gl, vertexShader, fragmentShader) {
 		return program;
 	}
 
-	console.log(gl.getprogramParamsLog(program));
+	console.log(gl.getProgramInfoLog(program));
 	gl.deleteProgram(program);
 }
 
